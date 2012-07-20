a15ceb648d232cd00df52a97ab19979f88a37470
wsky <wskyhx@gmail.com>
2012-07-20 14:05:10 +0800

    add MSTest package

--- dotnet/lib/pack.bat
+++ dotnet/lib/pack.bat
@@ -1,10 +1,12 @@
-..\..\external-work\tools\nuget pack CodeSharp.Package.AspNetMvc3.3.0.nuspec
-..\..\external-work\tools\nuget pack CodeSharp.Package.AspNetWebPage.1.0.nuspec
+..\..\..\work-tool\nuget pack CodeSharp.Package.AspNetMvc3.3.0.nuspec
+..\..\..\work-tool\nuget pack CodeSharp.Package.AspNetWebPage.1.0.nuspec
+..\..\..\work-tool\nuget pack CodeSharp.Package.MSTest.10.0.nuspec
 
-..\..\external-work\tools\nuget setApiKey 639c61cf-6e89-4a98-a69a-31b6fa0e2378
+..\..\..\work-tool\nuget  setApiKey 639c61cf-6e89-4a98-a69a-31b6fa0e2378 
+..\..\..\work-tool\nuget push CodeSharp.Package.AspNetMvc3.3.0.nupkg
+..\..\..\work-tool\nuget push CodeSharp.Package.AspNetWebPage.1.0.nupkg
+..\..\..\work-tool\nuget push CodeSharp.Package.MSTest.10.0.nupkg
 
-..\..\external-work\tools\nuget push CodeSharp.Package.AspNetMvc3.3.0.nupkg
-..\..\external-work\tools\nuget push CodeSharp.Package.AspNetWebPage.1.0.nupkg
-
-..\..\external-work\tools\nuget push CodeSharp.Package.AspNetMvc3.3.0.nupkg -s http://nuget.incooper.net/ codesharp
-..\..\external-work\tools\nuget push CodeSharp.Package.AspNetWebPage.1.0.nupkg -s http://nuget.incooper.net/ codesharp
+..\..\..\work-tool\nuget push CodeSharp.Package.AspNetMvc3.3.0.nupkg -s http://nuget.icodesharp.com/ codesharp
+..\..\..\work-tool\nuget push CodeSharp.Package.AspNetWebPage.1.0.nupkg -s http://nuget.icodesharp.com/ codesharp
+..\..\..\work-tool\nuget push CodeSharp.Package.MSTest.10.0.nupkg -s http://nuget.icodesharp.com/ codesharp
